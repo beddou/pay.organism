dd2736476f4365f248ee961020ddae580b0a05b0
beddou said <beddousaid@gmail.com>
2022-04-17 11:33:00 +0100

    1st commit

---
+++ src/main/java/com/pay/organism/OrganismApplication.java
@@ -0,0 +1,13 @@
+package com.pay.organism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay/organism/OrganismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.organism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
